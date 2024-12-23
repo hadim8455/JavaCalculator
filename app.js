d48ddad60da8af9404362b8f649ddabff9db7796
Muhammad Hadi <hadim8455@gmail.com>
2024-12-23 21:38:48 +0500

    code completed

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
         // calculator
 
-// var val1 = +prompt("val1: ");
-// var val2 = +prompt("val2: ");
-// var operator = prompt("+,-,*,/")
+var val1 = +prompt("val1: ");
+var val2 = +prompt("val2: ");
+var operator = prompt("+,-,*,/")
 
-// if(operator == "+" ){
-//     console.log("Add:", val1 + val2);
-// }
-// else if(operator == "-"){
-//     console.log("sub", val1 - val2);
-// }
-// else if(operator == "*"){
-//     console.log("Mul", val1 * val2);
-// }
-// else if(operator == "/"){
-//     console.log("Div", val1 / val2);
-// }
-// else{
-//     console.log("Invalid Number");
-// }
+if(operator == "+" ){
+    console.log("Add:", val1 + val2);
+}
+else if(operator == "-"){
+    console.log("sub", val1 - val2);
+}
+else if(operator == "*"){
+    console.log("Mul", val1 * val2);
+}
+else if(operator == "/"){
+    console.log("Div", val1 / val2);
+}
+else{
+    console.log("Invalid Number");
+}
